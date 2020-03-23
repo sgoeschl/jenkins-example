bc8a9376461d452100b0b9c611a706c06e0ee31e
Siegfried Goeschl <siegfried.goeschl@gmail.com>
2020-03-23 18:48:43 +0100

    Initial import

--- vars/bom.groovy
+++ vars/bom.groovy
@@ -2,19 +2,15 @@
 
 @NonCPS
 List<Map<String, String>> toJobData(String csv) {
-    final List<Map<String, String>> result = []
+    final List<Map<String, String>> list = []
     csv?.eachLine { line ->
-        // skip header line
+        // skip header line, e.g. "APPLICATION,VERSION"
         if (!line.trim().isEmpty() && !line.startsWith("APPLICATION")) {
             final List<String> tokens = line.trim().tokenize(",;")
-            final Map<String, String> job = [:]
-            job["APPLICATION"] = tokens[0]
-            job["VERSION"] = tokens[1]
-            job["GIT_REF"] = "master"
-            result.add(job)
+            list.add(["APPLICATION":tokens[0], "VERSION":tokens[1], "GIT_REF":"master"])
         }
     }
-    return result
+    return list
 }
 
 return this
