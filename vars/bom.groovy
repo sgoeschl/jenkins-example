60757c59f873d4c41eb720679bca032249f4cb30
Siegfried Goeschl <siegfried.goeschl@gmail.com>
2020-03-23 19:03:24 +0100

    Initial import

--- vars/bom.groovy
+++ vars/bom.groovy
@@ -1,13 +1,26 @@
 #!groovy
 
+/**
+ * Parse a CSV and return a list of maps with job parameters.
+ * The following columns are supported:
+ * <ol>
+ *     <li>APPLICATION</li>
+ *     <li>VERSION</li>
+ * </ol>
+ * @param csv CSV containing the application names &amp: versions to be deployed.
+ * @return list of maps with job parameters
+ */
 @NonCPS
-List<Map<String, String>> toJobData(String csv) {
+List<Map<String, String>> toApplicationJobParameterList(String csv) {
     final List<Map<String, String>> list = []
     csv?.eachLine { line ->
         // skip header line, e.g. "APPLICATION,VERSION"
         if (!line.trim().isEmpty() && !line.startsWith("APPLICATION")) {
             final List<String> tokens = line.trim().tokenize(",;")
-            list.add(["APPLICATION":tokens[0], "VERSION":tokens[1], "GIT_REF":"master"])
+            final String application = tokens[0].trim()
+            final String version = tokens[1].trim()
+            final String gitRef = "master"
+            list.add(["APPLICATION": application, "VERSION": version, "GIT_REF": gitRef])
         }
     }
     return list
