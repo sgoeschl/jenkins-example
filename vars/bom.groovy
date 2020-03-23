616d5c98c8f968eab4311f4d15f3a6793268eace
Siegfried Goeschl <siegfried.goeschl@gmail.com>
2020-03-23 20:38:15 +0100

    Initial import

--- vars/bom.groovy
+++ vars/bom.groovy
@@ -8,12 +8,29 @@
  *     <li>VERSION</li>
  * </ol>
  * @param bomVersion BOM version being deployed
+ * @param bomCsvFile CSV containing the application names &amp: versions to be deployed
  * @return list of maps with job parameters
  */
-// @NonCPS
+@NonCPS
 List<Map<String, String>> toApplicationJobParameterList(String bomVersion) {
-    def rootDir = pwd()
-    final File bomCsvFile = new File("./bom/${bomVersion}.csv")
+    final File bomDir = new File(".")
+    final File bomCsvFile = new File(bomDir, "./bom/${bomVersion}.csv");
+    toApplicationJobParameterList(bomCsvFile)
+}
+
+/**
+ * Parse a CSV and return a list of maps with job parameters.
+ * The following columns are supported:
+ * <ol>
+ *     <li>APPLICATION</li>
+ *     <li>VERSION</li>
+ * </ol>
+ * @param bomVersion BOM version being deployed
+ * @param bomCsvFile CSV containing the application names &amp: versions to be deployed
+ * @return list of maps with job parameters
+ */
+@NonCPS
+List<Map<String, String>> toApplicationJobParameterList(File bomCsvFile) {
     final List<Map<String, String>> list = []
     bomCsvFile.text.eachLine { line ->
         // skip header line, e.g. "APPLICATION,VERSION"
